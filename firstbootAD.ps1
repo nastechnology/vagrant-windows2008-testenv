0efe352221776c5bc43b50ddeffc8b0ecde89b58
Mark Myers <technology@napoleonareaschools.org>
2014-11-11 15:18:03 -0500

    Added new approach to DCPROMO

--- firstbootAD.ps1
+++ firstbootAD.ps1
@@ -3,36 +3,12 @@ Import-Module Servermanager
 Add-WindowsFeature AD-Domain-Services,ADDS-Domain-Controller 
 
 # Run dcpromo
-#Create an unattend.txt file to automate the DCPROMO process
-$unattend = "c:\unattend.txt"
-if(!(test-path $unattend)) { new-item $unattend -type file }
+$dcpromo = "dcpromo /unattend:C:\vagrant\nacs_dev_ad.txt"
 
-$content = @(
-    "[DCInstall]"
-    "ReplicaOrNewDomain=Domain"
-    "NewDomain=Forest"
-    "NewDomainDNSName=dc.nacs.dev"
-    "ForestLevel=4"
-    "DomainNetbiosName=dc"
-    "DomainLevel=4"
-    "InstallDNS=Yes"
-    "ConfirmGc=Yes"
-    "CreateDNSDelegation=No"
-    'DatabasePath="C:\Windows\NTDS"'
-    'LogPath="C:\Windows\NTDS"'
-    'SYSVOLPath="C:\Windows\SYSVOL"'
-    'SafeModeAdminPassword="T2Gt1wU70!"'
-    "RebootOnCompletion=Yes"
-)
-
-#out-file -filepath $unattend -inputobject $content
-write-outpu $content | out-file $unattend
-
-$dcpromo = "dcpromo /unattend:$unattend"
+invoke-expression -command $dcpromo
 
 # Create Admin user
 New-ADUser nasadmin -givenanem NACS -surname admin -AccountPassword (ConvertTo-SecureString "T2Gt1wU70!" -AsPlainText -force) -enabled $true
 
-invoke-expression -command $dcpromo
 
 
