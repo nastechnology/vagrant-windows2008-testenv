9261dce22c036deba489b4b2a7d17db37b24895e
Mark Myers <technology@napoleonareaschools.org>
2014-11-11 15:42:20 -0500

    Removed create AD User it should use vagrant user

--- firstbootAD.ps1
+++ firstbootAD.ps1
@@ -7,8 +7,5 @@ $dcpromo = "dcpromo /unattend:C:\vagrant\nacs_dev_ad.txt"
 
 invoke-expression -command $dcpromo
 
-# Create Admin user
-New-ADUser nasadmin -givenanem NACS -surname admin -AccountPassword (ConvertTo-SecureString "T2Gt1wU70!" -AsPlainText -force) -enabled $true
-
 
 
