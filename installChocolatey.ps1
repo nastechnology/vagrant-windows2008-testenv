b895215fb0d9cc5b9a43c8fe34517d78da3ead5d
Mark Myers <technology@napoleonareaschools.org>
2014-11-12 12:09:25 -0500

    Added command paste and google chrome install to install chocolatey

--- installChocolatey.ps1
+++ installChocolatey.ps1
@@ -1,1 +1,5 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+invoke-expression -Command "choco install wincommandpaste"
+
+invoke-expression -Command "choco install googlechrome"
